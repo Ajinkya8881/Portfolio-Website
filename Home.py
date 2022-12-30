99b7d35a13f16d8bf50aefc8dd4888dec62f9bd1
__white_walker__ <aj.kolhe9657@gmail.com>
2022-12-30 18:32:46 +0530

    send emails using form

--- Home.py
+++ Home.py
@@ -36,6 +36,3 @@ with col4:
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source code]({row['url']})")
-
-
-
--- send_email.py
+++ send_email.py
@@ -1,21 +1,16 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "aj.kolhe9657@gmail.com"
-password = "pbknnbrpibrmpkrc"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "aj.kolhe9657@gmail.com"
+    password = "pbknnbrpibrmpkrc"
+    receiver = "aj.kolhe9657@gmail.com"
+    context = ssl.create_default_context()
 
-receiver = "aj.kolhe8445@gmail.com"
-context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-message = """\
-Subject: Hi!
-How are you?
-bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context)  as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
 
